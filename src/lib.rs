8762e373d9ccc2bc0c58ec726c4c2fd4ed592578
Rasib0 <rasibnadeem101@gmail.com>
2024-02-19 14:28:09 +0500

    added TokenTypes

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,76 @@
-use std::fs;
 use anyhow::Result;
+use std::fs;
 
-pub fn compile(file: String) -> Result<()>
-{
-	let source = fs::read_to_string(file)?;
-	let stream = source.chars();
-	let tokens = lex(stream)?;
-	let syntax = parse(tokens)?;
-	let target = translate(syntax);
-	Ok(println!("{target}"))
+pub fn compile(file: String) -> Result<()> {
+    let source = fs::read_to_string(file)?;
+    let stream = source.chars();
+    let tokens = lex(stream)?;
+    let syntax = parse(tokens)?;
+    let target = translate(syntax);
+    Ok(println!("{target}"))
 }
 
-struct Token
-{
+struct Token {}
 
+enum TokenType {
+    // Single-character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals
+    Identifier,
+    String,
+    Number,
+
+    // Keywords
+    And,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    True,
+    While,
+    // Class,
+    // This,
+    // Mut,
+    Eof,
 }
 
-struct Tree
-{
+struct Tree {}
 
+fn lex(stream: impl Iterator<Item = char>) -> Result<Vec<Token>> {
+    let mut stream = stream.enumerate().peekable();
+    unimplemented!()
 }
 
-fn lex(stream: impl Iterator<Item = char>) -> Result<Vec<Token>>
-{
-	let mut stream = stream.enumerate().peekable();
-	unimplemented!()
+fn parse(tokens: Vec<Token>) -> Result<Tree> {
+    unimplemented!()
 }
 
-fn parse(tokens: Vec<Token>) -> Result<Tree>
-{
-	unimplemented!()
-}
-
-fn translate(syntax: Tree) -> String
-{
-	unimplemented!()
+fn translate(syntax: Tree) -> String {
+    unimplemented!()
 }
