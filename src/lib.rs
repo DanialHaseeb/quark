b5a611a4c75083a2facbd9f95c761010d9fd9e25
Danial <76565771+DanialHaseeb@users.noreply.github.com>
2024-02-20 06:28:26 +0500

    Reformatted

--- src/lib.rs
+++ src/lib.rs
@@ -1,76 +1,81 @@
 use anyhow::Result;
 use std::fs;
 
-pub fn compile(file: String) -> Result<()> {
-    let source = fs::read_to_string(file)?;
-    let stream = source.chars();
-    let tokens = lex(stream)?;
-    let syntax = parse(tokens)?;
-    let target = translate(syntax);
-    Ok(println!("{target}"))
+pub fn compile(file: String) -> Result<()>
+{
+	let source = fs::read_to_string(file)?;
+	let stream = source.chars();
+	let tokens = lex(stream)?;
+	let syntax = parse(tokens)?;
+	let target = translate(syntax);
+	Ok(println!("{target}"))
 }
 
 struct Token {}
 
-enum TokenType {
-    // Single-character tokens
-    LeftParen,
-    RightParen,
-    LeftBrace,
-    RightBrace,
-    Comma,
-    Dot,
-    Minus,
-    Plus,
-    Semicolon,
-    Slash,
-    Star,
+enum TokenType
+{
+	// Single-character tokens
+	LeftParen,
+	RightParen,
+	LeftBrace,
+	RightBrace,
+	Comma,
+	Dot,
+	Minus,
+	Plus,
+	Semicolon,
+	Slash,
+	Star,
 
-    // One or two character tokens
-    Bang,
-    BangEqual,
-    Equal,
-    EqualEqual,
-    Greater,
-    GreaterEqual,
-    Less,
-    LessEqual,
+	// One or two character tokens
+	Bang,
+	BangEqual,
+	Equal,
+	EqualEqual,
+	Greater,
+	GreaterEqual,
+	Less,
+	LessEqual,
 
-    // Literals
-    Identifier,
-    String,
-    Number,
+	// Literals
+	Identifier,
+	String,
+	Number,
 
-    // Keywords
-    And,
-    Else,
-    False,
-    Fun,
-    For,
-    If,
-    Nil,
-    Or,
-    Print,
-    Return,
-    True,
-    While,
-    // Class,
-    // This,
-    // Mut,
-    Eof,
+	// Keywords
+	And,
+	Else,
+	False,
+	Fun,
+	For,
+	If,
+	Nil,
+	Or,
+	Print,
+	Return,
+	True,
+	While,
+	// Class,
+	// This,
+	// Mut,
+	Eof,
 }
 
 struct Tree {}
 
-fn lex(stream: impl Iterator<Item = char>) -> Result<Vec<Token>> {
-    let mut stream = stream.enumerate().peekable();
-    unimplemented!()
+fn lex(stream: impl Iterator<Item = char>) -> Result<Vec<Token>>
+{
+	let mut stream = stream.enumerate().peekable();
+	todo!()
 }
 
-fn parse(tokens: Vec<Token>) -> Result<Tree> {
-    unimplemented!()
+fn parse(tokens: Vec<Token>) -> Result<Tree>
+{
+  todo!()
 }
 
-fn translate(syntax: Tree) -> String {
-    unimplemented!()
+fn translate(syntax: Tree) -> String
+{
+  todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::{env, process};
 
-fn main() {
-    let file = env::args().skip(1).next().unwrap_or_else(|| {
-        eprintln!("No file provided");
-        process::exit(1);
-    });
+fn main()
+{
+	let file = env::args()
+		.skip(1)
+		.next()
+		.unwrap_or_else(| |
+		{
+			eprintln!("No file provided");
+			process::exit(1);
+		});
 
-    if let Err(message) = quark::compile(file) {
-        eprintln!("Error: {message}");
-        process::exit(1);
-    }
+	if let Err(message) = quark::compile(file)
+	{
+		eprintln!("Error: {message}");
+		process::exit(1);
+	}
 }
