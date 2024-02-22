dc15279dd5c4c9d072431ccafd2a91b569c14865
Danial <76565771+DanialHaseeb@users.noreply.github.com>
2024-02-22 12:09:47 +0500

    Updated lexer's `match` statement

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -27,9 +27,8 @@ where
             }
 
             // TODO: What about negative numbers?
-            '"' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
-                Literal(Literal::new(&mut stream))
-            }
+            '"' => Literal(Literal::new(&mut stream)),
+            _ if symbol.is_ascii_digit() => Literal(Literal::new(&mut stream)),
             _ => bail!("Unexpected symbol: {symbol}"),
         };
 
