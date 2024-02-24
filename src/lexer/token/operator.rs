c675ba88aac14f046ce28e3ce1060e2ad911e968
Rasib0 <rasibnadeem101@gmail.com>
2024-02-24 20:49:39 +0500

    added TwoChar Operators

--- src/lexer/token/operator.rs
+++ src/lexer/token/operator.rs
@@ -1,7 +1,16 @@
+use std::iter::Peekable;
+use DoubleCharOperator::*;
 use Operator::*;
+use SingleCharOperator::*;
 
 #[derive(Debug, PartialEq)]
 pub enum Operator {
+    SingleChar(SingleCharOperator),
+    DoubleChar(DoubleCharOperator),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum SingleCharOperator {
     Plus,
     Minus,
     Multiply,
@@ -12,27 +21,68 @@ pub enum Operator {
     Greater,
     Ampersand,
     Pipe,
-    Bang,
+    Not,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum DoubleCharOperator {
+    PlusEquals,
+    MinusEquals,
+    MultiplyEquals,
+    NotEquals,
+    DividEquals,
+    ModEquals,
+    EqualEquals,
+}
+
+pub trait MatchTrait<T>
+where
+    T: Iterator<Item = char>,
+{
+    fn next_if_matches(&mut self, c: char) -> bool;
+}
+
+impl<T> MatchTrait<T> for Peekable<T>
+where
+    T: Iterator<Item = char>,
+{
+    fn next_if_matches(&mut self, c: char) -> bool {
+        if let Some(&next_char) = self.peek() {
+            if next_char == c {
+                self.next();
+                return true;
+            }
+        }
+        false
+    }
 }
 
 impl Operator {
-    pub fn new<T>(stream: &mut T) -> Self
+    pub fn new<T>(stream: &mut Peekable<T>) -> Self
     where
         T: Iterator<Item = char>,
     {
-        // TODO: implement two-character operators like '==' and '!='
         match stream.next().unwrap() {
-            '+' => Plus,
-            '-' => Minus,
-            '*' => Multiply,
-            '/' => Divide,
-            '%' => Percent,
-            '=' => Equal,
-            '<' => Less,
-            '>' => Greater,
-            '&' => Ampersand,
-            '|' => Pipe,
-            '!' => Bang,
+            '+' if stream.next_if_matches('=') => DoubleChar(PlusEquals),
+            '-' if stream.next_if_matches('=') => DoubleChar(MinusEquals),
+            '*' if stream.next_if_matches('=') => DoubleChar(MultiplyEquals),
+            '!' if stream.next_if_matches('=') => DoubleChar(NotEquals),
+            '/' if stream.next_if_matches('=') => DoubleChar(DividEquals),
+            '%' if stream.next_if_matches('=') => DoubleChar(ModEquals),
+            '=' if stream.next_if_matches('=') => DoubleChar(EqualEquals),
+
+            '+' => SingleChar(Plus),
+            '-' => SingleChar(Minus),
+            '*' => SingleChar(Multiply),
+            '!' => SingleChar(Not),
+            '/' => SingleChar(Divide),
+            '%' => SingleChar(Percent),
+            '=' => SingleChar(Equal),
+
+            '<' => SingleChar(Less),
+            '>' => SingleChar(Greater),
+            '&' => SingleChar(Ampersand),
+            '|' => SingleChar(Pipe),
             _ => unreachable!(),
         }
     }
