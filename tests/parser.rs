3cec75d4da2455b38b7c2d92e8fc64fe38fbb1d4
Rasib0 <rasibnadeem101@gmail.com>
2024-03-25 10:06:17 +0500

    added a matrix specific test

--- tests/parser.rs
+++ tests/parser.rs
@@ -133,3 +133,15 @@ fn test_matrices_basic() {
 |1, 2];\n"
     );
 }
+
+#[test]
+fn test_matrices_expression_nesting() {
+    let input = "let A = [1 + 2, (3 + 2) || 3 + 4, 1 * (2/ 3)];";
+    let tokens = quark::lexer::lex(input.to_string()).unwrap();
+    let expression = quark::parser::parse(tokens).unwrap();
+    assert_eq!(
+        format!("{}", expression),
+        "let A = [(+ 1 2), (group (+ 3 2))|
+|(+ 3 4), (* 1 (group (/ 2 3)))];\n"
+    );
+}
